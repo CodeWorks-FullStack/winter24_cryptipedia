fc0cdd4a21d53428b7593021a91e794182e08832
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-20 11:19:02 -0700

    storing response data in appstate

---
+++ client/src/models/Cryptid.js
@@ -0,0 +1,15 @@
+export class Cryptid {
+  constructor(data) {
+    this.id = data.id
+    this.createdAt = new Date(data.createdAt)
+    this.updatedAt = new Date(data.updatedAt)
+    this.name = data.name
+    this.description = data.description
+    this.imgUrl = data.imgUrl
+    this.threatLevel = data.threatLevel
+    this.origin = data.origin
+    this.size = data.size
+    this.discovererId = data.discovererId
+    this.discoverer = data.discoverer
+  }
+}
--- client/src/services/CryptidsService.js
+++ client/src/services/CryptidsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Cryptid } from "@/models/Cryptid.js"
+import { AppState } from "@/AppState.js"
 
 class CryptidsService {
   async getCryptids() {
     const response = await api.get('api/cryptids')
     logger.log('GOT CRYPTIDS 👽👽👽', response.data)
+    const cryptids = response.data.map(cryptidPOJO => new Cryptid(cryptidPOJO))
+    AppState.cryptids = cryptids
   }
 }
 
