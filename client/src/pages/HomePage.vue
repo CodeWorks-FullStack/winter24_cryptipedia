ec97e9cdc03d7a3d7110cd73fd6c2b9105e7375c
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-20 11:04:25 -0700

    hue rotated cryptid cat

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -3,35 +3,42 @@
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container-fluid forest-bg">
+    <section class="row shadow-layer">
+      <div class="col-md-7 align-self-center">
+        <div class="p-5 text-light">
+          <h1>Terrestrials</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsum dolore culpa sint, dolor aliquam
+            blanditiis soluta minus commodi quos veritatis voluptas officiis quia distinctio esse, quam obcaecati
+            perspiciatis, sed quidem quasi. Obcaecati itaque consectetur laborum illo adipisci reprehenderit accusamus
+            amet iusto expedita doloribus! Non cupiditate vero itaque omnis. Consequuntur!</p>
+        </div>
+      </div>
+      <div class="col-md-5 align-self-end">
+        <img src="/public/img/cryptid_cat.png" alt="Cryptid cat" class="img-fluid">
+      </div>
+    </section>
   </div>
 </template>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
+.forest-bg {
+  background-image: url(https://images.unsplash.com/photo-1446553009413-64b9505cacb0?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  background-size: cover;
+}
 
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
+.shadow-layer {
+  backdrop-filter: blur(2px);
+  background-color: rgba(0, 0, 0, 0.5);
 
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
+
+  h1,
+  p {
+    text-shadow: 1px 1px black;
+  }
+
+  img {
+    filter: hue-rotate(-100deg);
   }
 }
 </style>
