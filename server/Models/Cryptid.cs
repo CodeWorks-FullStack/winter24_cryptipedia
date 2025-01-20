43d8f58566f283c3b262411f7c6ab0c90ca5c5ae
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-20 16:33:14 -0700

    notes

--- server/Models/Cryptid.cs
+++ server/Models/Cryptid.cs
@@ -2,15 +2,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace cryptipedia.Models;
 
-public class Cryptid : RepoItem<int>
+public class Cryptid : RepoItem<int> // int is the type of id
 {
-  // id, createdAt,updatedAt inherited from repo item
+  // id, createdAt, updatedAt inherited from repo item
   public string Name { get; set; }
   [MaxLength(10000)] public string Description { get; set; }
   [Url, MaxLength(3000)] public string ImgUrl { get; set; }
   [Range(0, 10)] public int ThreatLevel { get; set; }
-  public string Origin { get; set; }
   [Range(0, 10)] public int Size { get; set; }
+  public string Origin { get; set; }
   public string DiscovererId { get; set; }
   public Profile Discoverer { get; set; }
 }
--- server/Models/RepoItem.cs
+++ server/Models/RepoItem.cs
@@ -1,8 +1,10 @@
 namespace cryptipedia.Models;
 
 // abstract denotes that this class can never be instantiated, only inherited from
+// <T> represents that RepoItem will be supplied a Type argument when it is inherited from
 public abstract class RepoItem<T>
 {
+  // we can use the Type from T for our Id
   public T Id { get; set; }
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
