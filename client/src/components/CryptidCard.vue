c467f13681bba8b7e5bea5bb6754c4243ec63458
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-21 11:51:37 -0700

    placeholder on card for encounter count

--- client/src/components/CryptidCard.vue
+++ client/src/components/CryptidCard.vue
@@ -10,7 +10,13 @@ defineProps({
 <template>
   <RouterLink :to="{ name: 'CryptidDetails', params: { cryptidId: cryptid.id } }">
     <div class="cryptid-card text-light fs-4  mb-5" :style="{ backgroundImage: `url(${cryptid.imgUrl})` }">
-      <div class="inner-card d-flex align-items-end p-3">
+      <div class="inner-card d-flex flex-column justify-content-between p-3">
+        <div class="text-end">
+          <span title="Encountered by 0 humans">
+            <i class="mdi mdi-ufo-outline me-1"></i>
+            <span>0</span>
+          </span>
+        </div>
         <div>
           <span>{{ cryptid.displayId }}</span>
           <hr>
